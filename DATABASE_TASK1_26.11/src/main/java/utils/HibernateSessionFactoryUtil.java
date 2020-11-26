267e0db248bd7bc5ff987821875e5731827d04df
AlexParog <66374853+AlexParog@users.noreply.github.com>
2020-11-26 10:03:55 +0300

    Add files via upload

---
+++ DATABASE_TASK1_26.11/src/main/java/utils/HibernateSessionFactoryUtil.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.hibernate.SessionFactory;
+
+import javax.security.auth.login.Configuration;
+
+public class HibernateSessionFactoryUtil {
+    private  static SessionFactory sessionFactory;
+
+    private HibernateSessionFactoryUtil(){};
+
+    public  static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+
+            }
+        }
+    }
+}
